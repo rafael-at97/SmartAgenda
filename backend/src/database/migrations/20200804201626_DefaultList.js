6e4eb33faba49bfe70b97125af7aeb514889798f
Rosana <rosanasrib@gmail.com>
2020-08-04 20:41:10 -0300

    replace .delete() to .del()

--- backend/src/database/migrations/20200804201626_DefaultList.js
+++ backend/src/database/migrations/20200804201626_DefaultList.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
-    return knex('lists').insert({title:'00_DEFAULT_00', color: '0xffffff', parentID: 1});
+    return knex('lists').insert({title:'00_DEFAULT_00', color: 'ffffff', parentID: 1});
 };
 
 exports.down = function(knex) {
-    return  knex('lists').where('listID', 1).delete();
+    return knex('lists').where('listID', 1).del();
 };
